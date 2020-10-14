254921d588e6e20d4e8673e00873c7d29ae671c3
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2020-10-14 19:50:15 +0300

    decouple tree nav selected and expanded states

--- packages/@okta/vuepress-theme-prose/components/SidebarItem.vue
+++ packages/@okta/vuepress-theme-prose/components/SidebarItem.vue
@@ -2,16 +2,16 @@
     <li :class="{'subnav': link.subLinks}">
         <div class="link-wrap">
             <div v-if="link.path">
-                <router-link :to="link.path" exact @click="setData" class="tree-nav-link">{{link.title}}</router-link>
+                <router-link :to="link.path" exact class="tree-nav-link">{{link.title}}</router-link>
             </div>
 
             <div v-else>
-                <div :class="{'is-link':true, 'item-collapsable': true, 'router-link-active': iHaveChildrenActive}" @click="toggle">
-                    <svg viewBox="0 0 320 512" v-if="link.subLinks && !iHaveChildrenActive">
+                <div :class="{'is-link':true, 'item-collapsable': true, 'router-link-active': this.link.imActive}" @click="toggle">
+                    <svg viewBox="0 0 320 512" v-if="link.subLinks && !isExpanded">
                         <path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"/>
                     </svg>
 
-                    <svg viewBox="0 0 320 512" v-if="link.subLinks && iHaveChildrenActive">
+                    <svg viewBox="0 0 320 512" v-if="link.subLinks && isExpanded">
                         <path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"/>
                     </svg>
                     {{link.title}}
@@ -19,7 +19,7 @@
             </div>
         </div>
 
-        <ul v-if="link.subLinks" class="sections" v-show="iHaveChildrenActive">
+        <ul v-if="link.subLinks" class="sections" v-show="isExpanded">
             <SidebarItem v-for="sublink in link.subLinks" :key="sublink.title" :link="sublink" />
         </ul>
       </li>
@@ -34,7 +34,7 @@ export default {
     },
     data() {
         return {
-            iHaveChildrenActive: false
+            isExpanded: false,
         }
     },
     mounted() {
@@ -44,12 +44,12 @@ export default {
         'link'() {
             this.setData();
         },
-        'iHaveChildrenActive' (isActivated, _) {
-            if (isActivated && this.link.path) {
+        'isExpanded' (isExpanded, _) {
+            if (isExpanded && this.link.path) {
                 this.$el.scrollIntoView({
                     block: 'center'
                 });
-            } else if (isActivated) {
+            } else if (isExpanded) {
                 this.$el.scrollIntoView({
                     block: 'nearest'
                 });
@@ -58,10 +58,10 @@ export default {
     },
     methods: {
         toggle() {
-            this.iHaveChildrenActive = !this.iHaveChildrenActive
+            this.isExpanded = !this.isExpanded
         },
         setData: function() {
-            this.iHaveChildrenActive = Boolean(this.link.imActive);
+            this.isExpanded = Boolean(this.link.imActive);
         }
     }
 }
